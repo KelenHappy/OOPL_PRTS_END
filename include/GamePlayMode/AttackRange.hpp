7c9924a6612497e92daf7be62322ae45f620ae2c
Kelen <88041456+KelenHappy@users.noreply.github.com>
2025-04-01 22:33:32 +0800

    Create AttackRange.hpp

---
+++ include/GamePlayMode/AttackRange.hpp
@@ -0,0 +1,9 @@
+#ifndef ATTACK_RANGE_HPP
+#define ATTACK_RANGE_HPP
+#include <iostream>
+//丟入Character和Map
+template <typename T1, typename T2>
+void attack_range(T1 &thisC, T2 &that){
+    //thisC->SetRange();
+}
+#endif
