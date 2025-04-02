fedbf599f41bdab3cad00a539dd9b8fdc80857bc
KelenHappy <88041456+KelenHappy@users.noreply.github.com>
2025-04-02 14:50:52 +0800

    Update AppMap1017.cpp

--- src/AppMap1017.cpp
+++ src/AppMap1017.cpp
@@ -12,20 +12,39 @@ void App::LevelMain17() {
     }
 	// 設定角色出現與否
 
-	for (const auto& character : m_StartCharacter) {
-		if(character->GetHP() > 0){
-			character->SetLooping(true);
-		}
-		else{
+	for (size_t i = 0; i < m_StartCharacter.size(); ++i) {
+		auto& character = m_StartCharacter[i];
+		// 判斷是否活著
+		if (character->GetHP() <= 0) {
 			character->SetState(CharacterState::Die);
-			if(character->IfAnimationEnds()){
+			if (character->IfAnimationEnds()) {
 				character->SetLooping(false);
 				character->SetVisible(false);
+				// 從容器中移除死亡角色
+				m_StartCharacter.erase(m_StartCharacter.begin() + i);
+				--i;  // 刪除後需要調整索引
 			}
 		}
-        }
-	m_BugAs[0]->SetVisible(true);     
-	m_BugAs[0]->SetLooping(true);     
+		else {
+			character->SetLooping(true);
+		}
+		//判斷被放置
+
+		//判斷攻擊
+
+		//判斷Idle
+		if(character->GetState() != CharacterState::Default and
+			character->GetState() != CharacterState::Die and
+			character->GetState() != CharacterState::Attack and
+			character->GetState() != CharacterState::Start
+		){
+			character->SetLooping(true);
+			character->SetState(CharacterState::Idle);
+		}
+	}
+
+	m_BugAs[0]->SetVisible(true);
+	m_BugAs[0]->SetLooping(true);
 	m_BugAs[0]->SetState(EnemyState::Move);
 	/*if (m_Sussurro->GetHP() > 0){
 		m_Sussurro->SetVisible(true);
@@ -34,24 +53,26 @@ void App::LevelMain17() {
 	}
     //Debug();
 	//敵人重生
-	
-	
+
+
 	for (size_t i = 0; i < m_BugAs.size(); ++i) {
-		if(m_BugAs[i]->GetHP() > 0){
-			m_BugAs[i]->SetVisible(true);     
-			m_BugAs[i]->SetLooping(true);     
-			m_BugAs[i]->SetState(EnemyState::Move);
-		}
-		else{
-			m_BugAs[i]->SetState(EnemyState::Die);
-			if(m_BugAs[i]->IfAnimationEnds()){
-				m_BugAs[i]->SetVisible(false);     
-				m_BugAs[i]->SetLooping(false);	
+		auto& bug = m_BugAs[i];
+		if(bug->GetHP <= 0){
+			bug->SetState(CharacterState::Die);
+			if (bug->IfAnimationEnds()) {
+				bug->SetLooping(false);
+				bug->SetVisible(false);
+				// 從容器中移除死亡角色
+				m_BugAs.erase(m_BugAs.begin() + i);
+				--i;  // 刪除後需要調整索引
 			}
+		}
+		else {
+			bugr->SetLooping(true);
 		}
 	}
 
-	
+
 	}*/
     m_0107.Update();
 }
