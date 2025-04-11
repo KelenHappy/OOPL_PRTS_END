877f397e98a66bb1d54dbedb7aff7253b943d5df
Kelen <88041456+KelenHappy@users.noreply.github.com>
2025-04-11 23:32:02 +0800

    Update Thrower.hpp

--- include/Enemy/Thrower.hpp
+++ include/Enemy/Thrower.hpp
@@ -5,7 +5,7 @@
 class Thrower : public Enemy{
 public:
     //              EnemyName, int IdleCont, int AttackCont,int DieCont, int MoveCont
-    Thrower() : Enemy("Thrower", 61, 91, 61, 60){
+    Thrower() : Enemy("Thrower", 60, 91, 61, 59){
 		//int Health, int Attack, int Defend, int MagicDefend, int AttackSpeed
 		//, int AttackDistance, int AttackCastle, int MoveSpeed, int HeavyLevel, int HealthRecover
 		//, bool Dizzy, bool Sleep, bool Frozen
