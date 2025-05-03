ad5ab65c77e61997886197db054ce050daf9517d
Kelen <88041456+KelenHappy@users.noreply.github.com>
2025-05-03 15:55:04 +0800

    Update Mlynar.hpp

--- include/Character/Guard/Mlynar.hpp
+++ include/Character/Guard/Mlynar.hpp
@@ -20,12 +20,12 @@ public:
 		SetAttackRangeNumBuff(1);
 		//建立初始攻擊範圍
 		SetAttackRangeNum(2);
-		SetDefaultRange();
-		this->m_DefaultRange->push_back(std::vector<std::string>(5, "0"));
-		this->m_DefaultRange->push_back(std::vector<std::string>(5, "0"));
-		this->m_DefaultRange->push_back({"2", "1", "1", "0", "0"});
-		this->m_DefaultRange->push_back(std::vector<std::string>(5, "0"));
-		this->m_DefaultRange->push_back(std::vector<std::string>(5, "0"));
+
+		this->m_DefaultRange.push_back(std::vector<std::string>(5, "0"));
+		this->m_DefaultRange.push_back(std::vector<std::string>(5, "0"));
+		this->m_DefaultRange.push_back({"2", "1", "1", "0", "0"});
+		this->m_DefaultRange.push_back(std::vector<std::string>(5, "0"));
+		this->m_DefaultRange.push_back(std::vector<std::string>(5, "0"));
     }
 	std::string GetChineseName() override {return "瑪恩納";}
 
